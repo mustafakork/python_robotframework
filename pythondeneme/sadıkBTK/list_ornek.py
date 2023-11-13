04653e57a114d76184ff9b5fbcf701579d279786
mustafakork <mstfbsrysrykb60@gmail.com>
2023-11-13 13:27:32 +0300

    vers2

---
+++ pythondeneme/sadıkBTK/list_ornek.py
@@ -0,0 +1,11 @@
+aracList=['BMW','Mercedes', 'Mazda','Tofaş']
+
+print(len(aracList)) #4
+# 1- listenin ilk ve son elemanı
+print(aracList[0]) #
+print(aracList[len(aracList)-1])
+
+# 2- Mazdayı toyata ile değiştirin
+aracList[2]='toyata'
+print(aracList)
+
